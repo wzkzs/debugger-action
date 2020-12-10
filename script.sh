9a52e0b5cbf707abba4d93bc89e58d7aedf9e1c5
wzk <57616924+wzkzs@users.noreply.github.com>
2020-12-10 15:16:13 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -37,7 +37,7 @@ if [[ ! -z "$SLACK_WEBHOOK_URL" ]]; then
 fi
 
 # Wait for connection to close or timeout in 15 min
-timeout=$((360*60))
+timeout=$((15*60))
 while [ -S /tmp/tmate.sock ]; do
   sleep 1
   timeout=$(($timeout-1))
@@ -45,7 +45,7 @@ while [ -S /tmp/tmate.sock ]; do
   if [ ! -f /tmp/keepalive ]; then
     if (( timeout < 0 )); then
       echo Waiting on tmate connection timed out!
-     
+      exit 1
     fi
   fi
 done
